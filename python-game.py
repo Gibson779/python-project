c4598050e11661dc6f92af1e086634562d768668
Regan Gibson <Regan.Gibson@autocab.com>
2023-01-10 16:28:52 +0000

    test commit

---
+++ python-game.py
@@ -0,0 +1,91 @@
+import os
+import time
+from termcolor import colored
+
+# This is the Canvas class. It defines some height and width, and a
+# matrix of characters to keep track of where the TerminalScribes are moving
+class Canvas:
+    def __init__(self, width, height):
+        self._x = width
+        self._y = height
+        # This is a grid that contains data about where the
+        # TerminalScribes have visited
+        self._canvas = [[' ' for y in range(self._y)] for x in range(self._x)]
+
+    # Returns True if the given point is outside the boundaries of the Canvas
+    def hitsWall(self, point):
+        return point[0] < 0 or point[0] >= self._x or point[1] < 0 or point[1] >= self._y
+
+    # Set the given position to the provided character on the canvas
+    def setPos(self, pos, mark):
+        self._canvas[pos[0]][pos[1]] = mark
+
+    # Clear the terminal (used to create animation)
+    def clear(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    # Clear the terminal and then print each line in the canvas
+    def print(self):
+        self.clear()
+        for y in range(self._y):
+            print(' '.join([col[y] for col in self._canvas]))
+
+class TerminalScribe:
+    def __init__(self, canvas):
+        self.canvas = canvas
+        self.trail = '.'
+        self.mark = '*'
+        self.framerate = 0.05
+        self.pos = [0, 0]
+
+    def up(self):
+        pos = [self.pos[0], self.pos[1]-1]
+        if not self.canvas.hitsWall(pos):
+            self.draw(pos)
+
+    def down(self):
+        pos = [self.pos[0], self.pos[1]+1]
+        if not self.canvas.hitsWall(pos):
+            self.draw(pos)
+
+    def right(self):
+        pos = [self.pos[0]+1, self.pos[1]]
+        if not self.canvas.hitsWall(pos):
+            self.draw(pos)
+
+    def left(self):
+        pos = [self.pos[0]-1, self.pos[1]]
+        if not self.canvas.hitsWall(pos):
+            self.draw(pos)
+
+    def drawSquare(self, size):
+        i = 0
+        while i < size:
+            self.right()
+            i = i + 1
+        i = 0
+        while i < size:
+            self.down()
+            i = i + 1
+        i = 0
+        while i < size:
+            self.left()
+            i = i + 1
+        i = 0
+        while i < size:
+            self.up()
+            i = i + 1
+
+
+
+    def draw(self, pos):
+        self.canvas.setPos(self.pos, self.trail)
+        self.pos = pos
+        self.canvas.setPos(self.pos, colored(self.mark, 'red'))
+        self.canvas.print()
+        time.sleep(self.framerate)
+
+canvas = Canvas(30, 30)
+scribe = TerminalScribe(canvas)
+
+scribe.drawSquare(20)
